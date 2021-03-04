70a2e4441e9bd4c0fd65c651a38c622d987657d0
Bronislav Strava <xstrav01@vutbr.cz>
2021-03-04 08:44:56 +0100

    Přidejte soubory projektu.

---
+++ OOP-CV04/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_CV04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string testovaciText = "Toto je retezec predstavovany nekolika radky,\n"
+            + "ktere jsou od sebe oddeleny znakem LF (Line Feed).\n"
+            + "Je tu i nejaky ten vykricnik! Pro ucely testovani i otaznik?\n"
+            + "Toto je jen zkratka zkr. ale ne konec vety. A toto je\n"
+            + "posledni veta!";
+
+            StringStatistics st = new StringStatistics(testovaciText);
+            string[] subs = testovaciText.Split(' ');
+            Console.WriteLine("{0}", string.Join(" ", subs));
+
+            Console.WriteLine("\nPocet slov: " + st.wordCount());
+            Console.WriteLine("Pocet radku: " + st.lineCount());
+            Console.WriteLine("Pocet vet: " + st.sentenceCount());
+            Console.Write("Nejdelsi slova: ");
+            Console.WriteLine("{0}", string.Join(" , ", st.longestWords()));
+            Console.Write("Nejkratsi slova: ");
+            Console.WriteLine("{0}", string.Join(" , ", st.shortestWords()));
+            Console.Write("Nejcetnejsi slova: ");
+            Console.WriteLine("{0}", string.Join(" , ", st.mostFrequentElement()));
+            Console.Write("Slova dle abecedy: ");
+            Console.WriteLine("{0}", string.Join(" , ", st.sortAlfabeticaly()));
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ OOP-CV04/StringStatistics.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace OOP_CV04
+{
+    class StringStatistics
+    {
+        private String text = null;
+
+        public StringStatistics(String text) {
+            this.text = text;
+        }
+
+        public int wordCount() {
+            int wordCount = 0;
+            char[] separators = { ' ', ',', '?', '.', '\n' };
+            string[] subs = text.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            //Console.WriteLine("{0}", string.Join(" ", subs));
+            foreach (String item in subs)
+            {
+                wordCount += 1; 
+            }
+            return wordCount;
+        }
+
+        public int lineCount() {
+            int lineCount = 0;
+
+            char[] separators = { '\n' };
+            string[] subs = text.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            foreach (String item in subs)
+            {
+                lineCount += 1;
+            }
+
+            return lineCount;
+        }
+
+        public int sentenceCount()
+        {
+            int sentenceCount = 0;
+
+            //char[] separators = { '.', '!', '?' };
+            //string[] subs = text.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            string[] subs = Regex.Split(text, @"[.!?]\s[A-Z]");
+            foreach (String item in subs)
+            {
+                sentenceCount += 1;
+            }
+
+            return sentenceCount;
+        }
+
+        public String[] longestWords() {
+
+            char[] separators = { ',', '!', '?', '.', '(', ')', ' ', '\n' };
+            string[] subs = text.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            
+            List<string> list = new List<string>();
+            int pocet = 0;
+            foreach (String item in subs)
+            {
+                if (item.Length > pocet)
+                {
+                    pocet = item.Length;
+                    list.Clear();
+                    list.Add(item);
+                    
+                }
+                else if (item.Length == pocet) {
+                    list.Add(item);
+                }
+                
+            }
+            string[] longest = list.ToArray();
+            
+            return longest;
+        }
+        public String mostFrequentElement()
+        {
+            int longestCount = 1;
+            int count = 1;
+            string previous = "";
+            string mostFrequent = "";
+
+            foreach (string item in sortAlfabeticaly())
+            {
+                if (item == previous)
+                {
+                    count++;
+                    if (count > longestCount)
+                    {
+                        longestCount = count;
+                        mostFrequent = item;
+                    }
+
+                }
+                else if (previous != item)
+                {
+                    count = 1;
+                }
+                previous = item;
+            }
+
+            return mostFrequent;
+
+        }
+
+        public String[] shortestWords()
+        {
+
+            char[] separators = { ',', '!', '?', '.', '(', ')', ' ', '\n' };
+            string[] subs = text.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+            List<string> list = new List<string>();
+            int pocet = int.MaxValue;
+            foreach (String item in subs)
+            {
+                if (item.Length < pocet)
+                {
+                    pocet = item.Length;
+                    list.Clear();
+                    list.Add(item);
+
+                }
+                else if (item.Length == pocet)
+                {
+                    list.Add(item);
+                }
+
+            }
+            string[] shortest = list.ToArray();
+
+            return shortest;
+        }
+
+        public String[] sortAlfabeticaly()
+        {
+            char[] separators = { ',', '!', '?', '.', '(', ')', ' ', '\n' };
+            string[] subs = text.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+            Array.Sort(subs);
+
+            return subs;
+
+        }
+
+    }
+}
